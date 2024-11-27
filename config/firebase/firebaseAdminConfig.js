8a1344d76021cdc039f5d676558d2c139cc9a6a4
KoaDev <35237303+KoaDev@users.noreply.github.com>
2024-11-27 13:53:44 +0100

    27112024
    Fix authentification

--- config/firebase/firebaseAdminConfig.js
+++ config/firebase/firebaseAdminConfig.js
@@ -1,8 +1,9 @@
 const admin = require('firebase-admin');
 const { FIREBASE_DATABASE_URL, FIREBASE_ADMIN_KEY_FILE } = require('./constants');
+const { cert } = require("firebase-admin/app");
 
 admin.initializeApp({
-    credential: admin.credential.cert(FIREBASE_ADMIN_KEY_FILE),
+    credential: cert(FIREBASE_ADMIN_KEY_FILE),
     databaseURL: FIREBASE_DATABASE_URL,
 });
 
